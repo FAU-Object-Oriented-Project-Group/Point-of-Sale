d386059395bb7d24e918df1a1b753967bb0481f7
bee <batquartet@gmail.com>
2025-11-16 21:26:15 -0500

    Fix look-and-feel error.

--- src/module-info.java
+++ src/module-info.java
@@ -1,9 +1,3 @@
-/**
- * 
- */
-/**
- * 
- */
 module pointOfSale {
 	requires java.desktop;
 }
--- src/pointOfSale/App.java
+++ src/pointOfSale/App.java
@@ -4,15 +4,7 @@ import javax.swing.*;
 
 public class App {
 	
-	public static void main(String [] args) {
-		SwingUtilities.invokeLater(() -> {
-			try {
-				UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		});
-		
+	public static void main(String [] args) {		
 		POSFrame frame = new POSFrame();
 		frame.setVisible(true);
 	}	
