b3c813da76d8710e35dd7ca72e21266f499ae573
bee <batquartet@gmail.com>
2025-12-02 21:19:22 -0500

    Change look and feel back to system
    Doesn't this look a ton better?

--- src/pointOfSale/App.java
+++ src/pointOfSale/App.java
@@ -5,8 +5,16 @@
 
 package pointOfSale;
 
+import javax.swing.UIManager;
+
 public class App {
     public static void main(String[] args) {
+    	// Set the system look and feel
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {
+            System.err.println("Could not set system look and feel. Using default Metal L&F.");
+        }
         ReceiptModel model = new ReceiptModel();
         InvoiceStrategy strategy = new TextFileStrategy();
         POSFrame frame = new POSFrame();
